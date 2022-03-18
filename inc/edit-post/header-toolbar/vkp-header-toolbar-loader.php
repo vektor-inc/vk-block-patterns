c6570f9cb5422a12b2e4925b86e7b924bd12aea8
shimotmk <shimotmk1104@gmail.com>
2022-03-18 15:09:23 +0900

    fix / header-toolbarr typo

--- inc/edit-post/header-toolbar/vkp-header-toolbar-loader.php
+++ inc/edit-post/header-toolbar/vkp-header-toolbar-loader.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function vbp_admin_enqueue_scripts() {
   $asset = include  VBP_PATH . 'build/edit-post/header-toolbar/index.asset.php';
-  wp_enqueue_script( 'vk-patterns-header-toolbarr-js', VBP_URL . 'build/edit-post/header-toolbar/index.js', $asset['dependencies'], $asset['version'], true );
-  wp_enqueue_style( 'vk-patterns-header-toolbarr-css', VBP_URL . 'build/edit-post/header-toolbar/style-index.css' );
+  wp_enqueue_script( 'vk-patterns-header-toolbar-js', VBP_URL . 'build/edit-post/header-toolbar/index.js', $asset['dependencies'], $asset['version'], true );
+  wp_enqueue_style( 'vk-patterns-header-toolbar-css', VBP_URL . 'build/edit-post/header-toolbar/style-main.css' );
 }
 add_action( 'enqueue_block_editor_assets', 'vbp_admin_enqueue_scripts' );
