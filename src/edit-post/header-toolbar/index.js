3e641719a0f8cb15991ef4b332e8d9fa779eae33
shimotmk <shimotmk1104@gmail.com>
2022-03-25 16:22:07 +0900

    fix / pattern link オプション値が設定されていなかったら非表示

--- src/edit-post/header-toolbar/index.js
+++ src/edit-post/header-toolbar/index.js
@@ -3,46 +3,57 @@
  */
 import domReady from '@wordpress/dom-ready';
 import { render } from '@wordpress/element';
-import { subscribe } from '@wordpress/data'
+import { subscribe } from '@wordpress/data';
 import { getLocaleData } from '@wordpress/i18n';
 import { ExternalLink } from '@wordpress/components';
+
 import './style.scss';
+/*globals vkpOptions */
 
 const PatternsLink = () => {
-  return (
-    <ExternalLink href="https://patterns.vektor-inc.co.jp/">VK パターンライブラリ</ExternalLink>
-  );
+	return (
+		<ExternalLink href="https://patterns.vektor-inc.co.jp/">
+			VK パターンライブラリ
+		</ExternalLink>
+	);
 };
 
-domReady(() => {
-  // 言語設定を取得
-  // console.log(getLocaleData());
-  const lang = getLocaleData()[""]['lang'];
-  if (lang !== 'ja_JP') {
-    return
-  }
+domReady( () => {
+	const { showPatternsLink } = vkpOptions;
+	if ( ! showPatternsLink ) {
+		return;
+	}
 
-  const buttonRender = (() => {
-    const headerToolbar = document.querySelector('.edit-post-header-toolbar') || document.querySelector('.edit-site-header__toolbar')
-    if (!headerToolbar) {
-      return
-    }
+	// 言語設定を取得
+	// console.log(getLocaleData());
+	const lang = getLocaleData()[ '' ].lang;
+	if ( lang !== 'ja_JP' ) {
+		return;
+	}
 
-    const patternsLinkArea = document.createElement('div')
-    patternsLinkArea.classList.add('vk-patterns-header-toolbar')
+	const buttonRender = () => {
+		const headerToolbar =
+			document.querySelector( '.edit-post-header-toolbar' ) ||
+			document.querySelector( '.edit-site-header__toolbar' );
+		if ( ! headerToolbar ) {
+			return;
+		}
 
-    if (!headerToolbar.querySelector('.vk-patterns-header-toolbar')) {
-      render(<PatternsLink />, patternsLinkArea)
-      headerToolbar.appendChild(patternsLinkArea)
-    }
-  })
+		const patternsLinkArea = document.createElement( 'div' );
+		patternsLinkArea.classList.add( 'vk-patterns-header-toolbar' );
 
-  // 何かしらの変更があったら発火
-  // https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
-  subscribe(() => {
-    // console.log('subscribe');
-    if (!document.querySelector('.vk-patterns-header-toolbar')) {
-      buttonRender();
-    }
-  });
-});
+		if ( ! headerToolbar.querySelector( '.vk-patterns-header-toolbar' ) ) {
+			render( <PatternsLink />, patternsLinkArea );
+			headerToolbar.appendChild( patternsLinkArea );
+		}
+	};
+
+	// 何かしらの変更があったら発火
+	// https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
+	subscribe( () => {
+		// console.log('subscribe');
+		if ( ! document.querySelector( '.vk-patterns-header-toolbar' ) ) {
+			buttonRender();
+		}
+	} );
+} );
