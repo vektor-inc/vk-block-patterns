2eb0ffaa882482f89ef2dd4dfed9901300aa4a65
shimotmk <shimotmk1104@gmail.com>
2022-03-25 16:21:10 +0900

    fix / vkp-header-toolbar-loader format

--- inc/edit-post/header-toolbar/vkp-header-toolbar-loader.php
+++ inc/edit-post/header-toolbar/vkp-header-toolbar-loader.php
@@ -12,9 +12,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Load Scripts
  */
-function vbp_admin_enqueue_scripts() {
+function vbp_edit_post_enqueue_scripts() {
   $asset = include  VBP_PATH . 'build/edit-post/header-toolbar/index.asset.php';
-  wp_enqueue_script( 'vk-patterns-header-toolbar-js', VBP_URL . 'build/edit-post/header-toolbar/index.js', $asset['dependencies'], $asset['version'], true );
-  wp_enqueue_style( 'vk-patterns-header-toolbar-css', VBP_URL . 'build/edit-post/header-toolbar/style-main.css' );
+  wp_enqueue_script( 
+    'vk-patterns-header-toolbar-js', 
+    VBP_URL . 'build/edit-post/header-toolbar/index.js', 
+    $asset['dependencies'], 
+    $asset['version'], 
+    true 
+  );
+  $vbp_options = vbp_get_options();
+  wp_localize_script( 'vk-patterns-header-toolbar-js', 'vkpOptions', $vbp_options );
+
+  wp_enqueue_style( 
+    'vk-patterns-header-toolbar-css', 
+    VBP_URL . 'build/edit-post/header-toolbar/style-index.css' 
+  );
 }
-add_action( 'enqueue_block_editor_assets', 'vbp_admin_enqueue_scripts' );
+add_action( 'enqueue_block_editor_assets', 'vbp_edit_post_enqueue_scripts' );
