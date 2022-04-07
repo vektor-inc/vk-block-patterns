782bb7ad41986f06e986b54700be2bb4c2de0308
kurudrive <kurudrive@gmail.com>
2022-04-07 14:09:45 +0900

    phpcs

--- admin/admin.php
+++ admin/admin.php
@@ -52,10 +52,10 @@ function vbp_setting_page() {
 	$get_logo_html = '<img src="' . plugin_dir_url( __FILE__ ) . '/images/vk-block-patterns-logo_ol.svg" alt="VK Block Patterns" />';
 	$get_logo_html = apply_filters( 'vbp_logo_html', $get_logo_html );
 
-	$get_menu_html  = '<li><a href="#role-setting">' . __( 'Role Setting', 'vk-block-patterns' ) . '</a></li>';
-	$lang           = ( get_locale() === 'ja' ) ? 'ja' : 'en';
+	$get_menu_html = '<li><a href="#role-setting">' . __( 'Role Setting', 'vk-block-patterns' ) . '</a></li>';
+	$lang          = ( get_locale() === 'ja' ) ? 'ja' : 'en';
 	if ( 'ja' === $lang ) {
-		$get_menu_html  .= '<li><a href="#editor-setting">' . __( 'Editor Setting', 'vk-block-patterns' ) . '</a></li>';
+		$get_menu_html .= '<li><a href="#editor-setting">' . __( 'Editor Setting', 'vk-block-patterns' ) . '</a></li>';
 	}
 
 	Vk_Admin::admin_page_frame( $get_page_title, 'vbp_setting', $get_logo_html, $get_menu_html );
@@ -65,71 +65,71 @@ function vbp_setting_page() {
  * 設定項目の登録.
  */
 function vkp_show_patterns_register_settings() {
-  $properties_editor_settings = array();
-  $default_editor_settings    = array();
-  $default_option_settings    = array(
-    'role'         => array(
-      'type'    => 'string',
-      'default' => 'author',
-    ),
-    'showPatternsLink'         => array(
-      'type'    => 'boolean',
-      'default' => true,
-    ),
-  );
+	$properties_editor_settings = array();
+	$default_editor_settings    = array();
+	$default_option_settings    = array(
+		'role'             => array(
+			'type'    => 'string',
+			'default' => 'author',
+		),
+		'showPatternsLink' => array(
+			'type'    => 'boolean',
+			'default' => true,
+		),
+	);
 
-  foreach ($default_option_settings as $key => $value) {
-    $properties_editor_settings[$key] = array(
-      'type' => $value['type'],
-    );
-    $default_editor_settings[$key]    = $value['default'];
-  }
-  register_setting(
-    'vbp_setting',
-    'vk_block_patterns_options',
-    array(
-      'type'         => 'object',
-      'show_in_rest' => array(
-        'schema' => array(
-          'type'       => 'object',
-          'properties' => $properties_editor_settings,
-        ),
-      ),
-      'default'      => $default_editor_settings,
-    )
-  );
+	foreach ( $default_option_settings as $key => $value ) {
+		$properties_editor_settings[ $key ] = array(
+			'type' => $value['type'],
+		);
+		$default_editor_settings[ $key ]    = $value['default'];
+	}
+	register_setting(
+		'vbp_setting',
+		'vk_block_patterns_options',
+		array(
+			'type'         => 'object',
+			'show_in_rest' => array(
+				'schema' => array(
+					'type'       => 'object',
+					'properties' => $properties_editor_settings,
+				),
+			),
+			'default'      => $default_editor_settings,
+		)
+	);
 }
 add_action( 'init', 'vkp_show_patterns_register_settings' );
 
 /**
  * Enqueue scripts
- * 
+ *
  * @param mixed $hook_suffix
  * @return void
  */
 function vbp_admin_enqueue_scripts( $hook_suffix ) {
-  // 作成したオプションページ以外では読み込まない.
-  if ( 'settings_page_vk_block_patterns_options' !== $hook_suffix ) {
-    return;
-  }
+	// 作成したオプションページ以外では読み込まない.
+	if ( 'settings_page_vk_block_patterns_options' !== $hook_suffix ) {
+		return;
+	}
 
-  $asset = include VBP_PATH . 'build/admin/index.asset.php';
-  // Enqueue CSS dependencies.
-  foreach ( $asset['dependencies'] as $style ) {
-    wp_enqueue_style($style);
-  }
+	$asset = include VBP_PATH . 'build/admin/index.asset.php';
+	// Enqueue CSS dependencies.
+	foreach ( $asset['dependencies'] as $style ) {
+		wp_enqueue_style( $style );
+	}
 
-  wp_enqueue_script(
-    'vk-patterns-admin-js', 
-    VBP_URL . 'build/admin/index.js', 
-    $asset['dependencies'], 
-    $asset['version'], 
-    true
-  );
+	wp_enqueue_script(
+		'vk-patterns-admin-js',
+		VBP_URL . 'build/admin/index.js',
+		$asset['dependencies'],
+		$asset['version'],
+		true
+	);
 
-  // 画面読み込み時に保存値を localize_script を使って渡す.
-  // booleanは空'' falseまたは1 trueを渡す
-  $vbp_options = vbp_get_options();
-  wp_localize_script( 'vk-patterns-admin-js', 'vkpOptions', $vbp_options );
+	// 画面読み込み時に保存値を localize_script を使って渡す.
+	// boolean は 空 '' false または 1 true を渡す.
+	$vbp_options = vbp_get_options();
+	wp_localize_script( 'vk-patterns-admin-js', 'vkpOptions', $vbp_options );
 }
 add_action( 'admin_enqueue_scripts', 'vbp_admin_enqueue_scripts' );
