f2de33bfc5a9ecbc5edf7c9f34afc43cf3176e32
kurudrive <kurudrive@gmail.com>
2022-04-07 20:04:01 +0900

    [ Change version ] 1.16.1

--- vk-block-patterns.php
+++ vk-block-patterns.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Block Patterns
  * Plugin URI: https://github.com/vektor-inc/vk-block-patterns
  * Description: You can make and register your original custom block patterns.
- * Version: 1.16.0
+ * Version: 1.16.1
  * Requires at least: 5.8
  * Author:  Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
@@ -63,7 +63,7 @@ require dirname( __FILE__ ) . '/patterns-data/class-register-patterns-from-json.
 
 function vbp_get_options() {
 	$default = array(
-		'role' => 'author',
+		'role'             => 'author',
 		'showPatternsLink' => true,
 	);
 	$options = get_option( 'vk_block_patterns_options' );
