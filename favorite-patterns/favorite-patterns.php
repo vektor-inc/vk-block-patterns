5bb586aa5d987526ac90283f5497e47eb51a5269
drill-lancer <rickaddison7634@gmail.com>
2022-08-01 14:00:03 +0900

    fix: favorite patterns

--- favorite-patterns/favorite-patterns.php
+++ favorite-patterns/favorite-patterns.php
@@ -16,7 +16,7 @@ function vbp_get_pattern_api_data() {
             ),
         )
     );
-    return json_decode( $result['body'] );
+    return json_decode( $result['body'], true );
 }
 
 /**
@@ -24,12 +24,13 @@ function vbp_get_pattern_api_data() {
  */
 function vbp_register_favorite_patterns() {
     $pattern_api_data = vbp_get_pattern_api_data();
-    $patterns_data = $pattern_api_data->patterns;
+    $patterns_data = $pattern_api_data['patterns'];
+    
     if ( function_exists( 'mb_convert_encoding' ) ) {
         $patterns_data = mb_convert_encoding( $patterns_data, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
     }
+    
     $patterns = json_decode( $patterns_data, true );
-    //var_dump($patterns );
     register_block_pattern_category(
         'vk-pattern-favorites',
         array( 
