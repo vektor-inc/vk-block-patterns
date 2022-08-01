dc5f993de5fc1526bce8fb613a233917804f4267
drill-lancer <rickaddison7634@gmail.com>
2022-08-01 10:31:58 +0900

    add: block pattern

--- favorite-patterns/favorite-patterns.php
+++ favorite-patterns/favorite-patterns.php
@@ -6,7 +6,7 @@
 /**
  * API からデータを読み込み
  */
-function vbp_get_api_data() {
+function vbp_get_pattern_api_data() {
     $result = wp_remote_post(
         'https://test.patterns.vektor-inc.co.jp/wp-json/vk-patterns/v1/status',
         array(
@@ -19,3 +19,23 @@ function vbp_get_api_data() {
     return json_decode( $result['body'] );
 }
 
+/**
+ * パターンを登録
+ */
+function vbp_register_favorite_patterns() {
+    $pattern_api_data = vbp_get_pattern_api_data();
+    $patterns = $pattern_api_data['patterns'];
+    if ( ! empty( $patterns ) && is_array( $patterns ) ) {
+        foreach ( $patterns as $pattern ) {
+            register_block_pattern( 
+                $pattern['post_name'],
+                array(
+                    'title'      => $pattern['title'],
+                    'categories' => $pattern['categories'],
+                    'content'    => $pattern['content'],
+                )
+            );
+        }
+    }
+}
+add_action( 'init', 'vbp_register_favorite_patterns' );
