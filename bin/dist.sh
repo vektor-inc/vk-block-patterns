4547e87edce6a1ba87b6938e5f8e4be703804ef2
drill-lancer <rickaddison7634@gmail.com>
2023-05-19 03:27:47 +0900

    fix: dist

--- bin/dist.sh
+++ bin/dist.sh
@@ -10,8 +10,10 @@ src_dir="${dist_dir}/${PLUGIN_NAME}"
 ZIPBALL="${dist_dir}/${PLUGIN_NAME}.zip"
 
 [[ -e "${dist_dir}" ]] || mkdir "${dist_dir}"
-[[ -e "${src_dir}" ]] || mkdir "${src_dir}"
+[[ -e "${src_dir}" ]] && rm -r "${src_dir}"
 [[ -e "${ZIPBALL}" ]] && rm -r "${ZIPBALL}"
+
+rsync -av "${PLUGIN_DIR}/" "${src_dir}/" --exclude="dist/" --exclude-from='.distignore'
 
 cd "${dist_dir}"
 
