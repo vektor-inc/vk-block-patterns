d7a20e5522dc4e0817f9c7ba6d2f9e2ccd6b4b31
kurudrive <kurudrive@gmail.com>
2025-01-30 16:13:11 +0900

    [ Change version ] 1.32.4.1

--- vk-block-patterns.php
+++ vk-block-patterns.php
@@ -3,8 +3,8 @@
  * Plugin Name: VK Block Patterns
  * Plugin URI: https://github.com/vektor-inc/vk-block-patterns
  * Description: You can make and register your original custom block patterns.
- * Version: 1.32.4.0
- * Requires at least: 6.2
+ * Version: 1.32.4.1
+ * Requires at least: 6.4
  * Author:  Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
  * Text Domain: vk-block-patterns
